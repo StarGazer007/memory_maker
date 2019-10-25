760ed2a25b5c0bb629cfa1276513869305596c38
Lisa Rodgers <lisa@wdthemes.com>
2019-10-24 23:50:49 -0400

    Modified the newBoard

--- js/app.js
+++ js/app.js
@@ -1,26 +1,78 @@
 
+$(document).ready(function(){
 
-var memory_array = ['1/16','1/4', '5/16', '11/32', '3/8', '7/16', '1/2', '9/16', '5/8', '11/16', '3/4', '13/16', '7/8', '15/16','1','4','6','8','10','12','14','16','18'];
+     console.log("The DOM has loaded");
 
-var memory_values = [];
-var memory_title_ids =[];
-var tiles_flipped = 0;
-Array.prototype.memory_title_shuffle = function (){
-  var i = this.length, j, temp;
-  while(--i > 0 ){
-    j= Math.floor(Math.random() * (i+1));
-    temp = this[j];
-    this[j] = this[i];
-    this[i] = temp;
-  }
+     newBoard();
 
-  function newBoard(){
-    tiles_flipped = 0;
-    var output = 'test';
-    memory_array.memory_tile_shuffle();
-    for (var i=0; i < memory_array.length; i++){
 
-    }
+     var memory_array = ['1/16','1/4', '5/16', '11/32', '3/8', '7/16', '1/2', '9/16', '5/8', '11/16', '3/4', '13/16', '7/8', '15/16','1','4','6','8','10','12','14','16','18'];
 
-    document.getElementById('memory_board').innerHTML = output;
-  }
+     var memory_values = [];
+     var memory_title_ids =[];
+     var tiles_flipped = 0;
+
+      function shuffle(memory_array){
+           var i = this.length, j, temp;
+               while(--i > 0 ){
+                    j= Math.floor(Math.random() * (i+1));
+                    temp = this[j];
+                    this[j] = this[i];
+                    this[i] = temp;
+               }
+     }
+     function newBoard(){
+       tiles_flipped = 0;
+       var output = '';
+       shuffle();
+       for (var i=0; i < memory_array.length; i++){
+            output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+       }
+
+       document.getElementById('memory_board').innerHTML = output;
+     }
+
+     function memoryFlipTile(tile,val){
+	if(tile.innerHTML == "" && memory_values.length < 2){
+		tile.style.background = '#FFF';
+		tile.innerHTML = val;
+		if(memory_values.length == 0){
+			memory_values.push(val);
+			memory_tile_ids.push(tile.id);
+		} else if(memory_values.length == 1){
+			memory_values.push(val);
+			memory_tile_ids.push(tile.id);
+			if(memory_values[0] == memory_values[1]){
+				tiles_flipped += 2;
+				// Clear both arrays
+				memory_values = [];
+            	memory_tile_ids = [];
+				// Check to see if the whole board is cleared
+				if(tiles_flipped == memory_array.length){
+					alert("Board cleared... generating new board");
+					document.getElementById('memory_board').innerHTML = "";
+					newBoard();
+				}
+			} else {
+				function flip2Back(){
+				    // Flip the 2 tiles back over
+				    var tile_1 = document.getElementById(memory_tile_ids[0]);
+				    var tile_2 = document.getElementById(memory_tile_ids[1]);
+				    tile_1.style.background = 'url(tile_bg.jpg) no-repeat';
+            	    tile_1.innerHTML = "";
+				    tile_2.style.background = 'url(tile_bg.jpg) no-repeat';
+            	    tile_2.innerHTML = "";
+				    // Clear both arrays
+				    memory_values = [];
+            	    memory_tile_ids = [];
+				}
+				setTimeout(flip2Back, 700);
+			}
+		}
+	}
+}
+
+
+
+
+});
